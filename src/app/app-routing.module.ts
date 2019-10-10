6f732e05cf2b6b3f8af4bafc2341d8d16fc5075c
Han Jung <han.jung@finra.org>
2019-10-10 17:05:23 -0400

    navigating page faster

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DeliComponent } from './components/deli/deli.component';
+import { GrillComponent } from './components/grill/grill.component';
 
 
 const routes: Routes = [{
+  path : 'home/deli',
+  component: DeliComponent
+},{
+  path : 'home/grill',
+  component: GrillComponent
+},{
   path : '**',
   component: HomeComponent
 }];
